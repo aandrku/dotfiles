3ad96123211799949788fd4bfb296d5f655e8320
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-21 00:08:34 -0700

    nvim: change style of the smear-cursor

--- nvim/plugin/smear-cursor.lua
+++ nvim/plugin/smear-cursor.lua
@@ -1,11 +1,5 @@
 require('smear_cursor').setup({
-	stiffness = 0.8,
-	trailing_stiffness = 0.2,
-	distance_stop_animating = 0.8,
-	gamma = 2.2,
-	-- hide_target_hack = true,
-	-- cursor_color = '#8b34d1',
-	cursor_color = '#9da1a6',
-	legacy_computing_symbols_support = true,
-	smear_insert_mode = true,
+	stiffness = 0.5,
+	trailing_stiffness = 0.49,
+	never_draw_over_target = false,
 })
