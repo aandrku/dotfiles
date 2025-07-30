95f14a3cc197bfadfca78f1df5b3725ee3ce0f43
Andrii Sozonik <sozonik04.av@gmail.com>
2025-07-30 14:43:54 -0700

    nvim: clean up config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,26 @@
-require('options')
-require('key_mappings')
-require('folding')
-require('go-auto-fmtvet')
-require('sql')
-require('templ-format')
+vim.g.mapleader = ' '
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.scrolloff = 16
+vim.g.netrw_longlist = 1
+vim.g.netrw_liststyle = 1
+
+
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>fe', function() vim.cmd('Ex') end)
+vim.keymap.set('n', '<C-q>', function() vim.cmd('wq') end)
+vim.keymap.set('n', '<C-s>', function() vim.cmd('w') end)
+vim.keymap.set('v', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>v', '"*p')
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+
+require('gofmt')
 require('conform-format')
--- nvim/lua/sql.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = "afiedt.buf",  -- Replace with your specific file
-    callback = function()
-        vim.bo.filetype = "sql"  -- Set the desired filetype
-    end,
-})
