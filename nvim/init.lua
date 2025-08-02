fdec015c49211c26894dc604ba4356e7e14685b0
Andrii Sozonik <sozonik04.av@gmail.com>
2025-08-01 17:31:48 -0700

    nvim: move to structured lazy.nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,122 +22,11 @@ vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>')
 vim.keymap.set('n', '<leader>x', ':.lua<CR>')
 vim.keymap.set('v', '<leader>x', ':lua<CR>')
 
+-- plugins
+require("config.lazy")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-
-
--- Setup lazy.nvim
-require("lazy").setup({
-	spec = {
-		{ "vague2k/vague.nvim" },
-		{ "m4xshen/autoclose.nvim" },
-		{ "neovim/nvim-lspconfig" },
-		{ "echasnovski/mini.nvim" },
-		{
-			"nvim-treesitter/nvim-treesitter",
-			branch = 'master',
-			lazy = false,
-		},
-		{
-			'stevearc/oil.nvim',
-			---@module 'oil'
-			---@type oil.SetupOpts
-			opts = {},
-			-- Optional dependencies
-			dependencies = { { "echasnovski/mini.icons", opts = {} } },
-			-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-			-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-			lazy = false,
-		},
-		{
-			'saghen/blink.cmp',
-			-- optional: provides snippets for the snippet source
-			dependencies = { 'rafamadriz/friendly-snippets' },
-
-			-- use a release tag to download pre-built binaries
-			version = '1.*',
-			-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-			-- build = 'cargo build --release',
-			-- If you use nix, you can build from source using latest nightly rust with:
-			-- build = 'nix run .#build-plugin',
-
-			---@module 'blink.cmp'
-			---@type blink.cmp.Config
-			opts = {
-				-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-				-- 'super-tab' for mappings similar to vscode (tab to accept)
-				-- 'enter' for enter to accept
-				-- 'none' for no mappings
-				--
-				-- All presets have the following mappings:
-				-- C-space: Open menu or open docs if already open
-				-- C-n/C-p or Up/Down: Select next/previous item
-				-- C-e: Hide menu
-				-- C-k: Toggle signature help (if signature.enabled = true)
-				--
-				-- See :h blink-cmp-config-keymap for defining your own keymap
-				keymap = { preset = 'default' },
-
-				appearance = {
-					-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-					-- Adjusts spacing to ensure icons are aligned
-					nerd_font_variant = 'mono'
-				},
-
-				signature = {
-					enabled = true
-				},
-
-				-- (Default) Only show the documentation popup when manually triggered
-				completion = { documentation = { auto_show = false } },
-
-				-- Default list of enabled providers defined so that you can extend it
-				-- elsewhere in your config, without redefining it, due to `opts_extend`
-				sources = {
-					default = { 'lsp', 'path', 'snippets', 'buffer' },
-				},
-
-				-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-				-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-				-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-				--
-				-- See the fuzzy documentation for more information
-				fuzzy = { implementation = "prefer_rust_with_warning" }
-			},
-			opts_extend = { "sources.default" }
-		}
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "vague" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
-})
-
-
-
--- setup colorscheme
-local vague = require("vague")
-vague.setup({
-	transparent = true,
-
-})
 vim.cmd("colorscheme vague")
+
 
 -- mini pick
 local mini_pick = require("mini.pick")
@@ -194,7 +83,3 @@ ts.setup({
 
 -- gofmt
 require("gofmt")
-
--- setup autoclose
-local autoclose = require("autoclose")
-autoclose.setup()
