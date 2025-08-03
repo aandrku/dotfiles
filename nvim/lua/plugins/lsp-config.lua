8d8668f9558308aaf1f726860f5158b6aa24c269
Andrii Sozonik <sozonik04.av@gmail.com>
2025-08-02 22:24:33 -0700

    nvim: add rust-analyzer config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,22 +1,40 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		-- Lua
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		vim.lsp.config['lua_ls'] = require("lsp.configs.lua")
-		vim.lsp.enable("lua_ls")
+  "neovim/nvim-lspconfig",
+  config = function()
+    -- Lua
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+    vim.lsp.config['lua_ls'] = require("lsp.configs.lua")
+    vim.lsp.enable("lua_ls")
 
 
-		-- tailwindcss
-		vim.lsp.enable("tailwindcss")
+    -- tailwindcss
+    vim.lsp.enable("tailwindcss")
 
-		-- templ
-		vim.lsp.enable("templ")
+    -- templ
+    vim.lsp.enable("templ")
 
-		-- Go
-		vim.lsp.config['gopls'] = {
-			capabilities = capabilities
-		}
-		vim.lsp.enable("gopls")
-	end
+    -- rust
+    vim.lsp.config['rust-analyzer'] = {
+      cmd = { "rust-analyzer" },
+      filetypes = { "rust" },
+      capabilities = capabilities,
+      settings = {
+        ['rust-analyzer'] = {
+          diagnostics = {
+            enable = false,
+            checkOnSave = {
+              command = "clippy",
+            }
+          }
+        }
+      }
+    }
+    vim.lsp.enable("rust-analyzer")
+
+    -- Go
+    vim.lsp.config['gopls'] = {
+      capabilities = capabilities
+    }
+    vim.lsp.enable("gopls")
+  end
 }
